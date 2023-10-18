4cdbfde84deb587fd68282960f248b6c1faa4e71
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-10-18 14:56:06 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+
+    AOS.init();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            $('header.header').addClass('sticky');
+        } else {
+            $('header.header').removeClass('sticky');
+        }
+    });
+
+    if ($(this).scrollTop() > 10) {
+        $('header.header').addClass('sticky');
+    } else {
+        $('header.header').removeClass('sticky');
+    }
+
+
+
+    var swiper = new Swiper(".trustedby-slider", {
+        slidesPerView: 6,
+        spaceBetween: 20,
+        speed: 3000,
+        loop: true,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+            },
+            576: {
+                slidesPerView: 3,
+            },
+            992: {
+                slidesPerView: 4,
+            },
+            1200: {
+                slidesPerView: 5,
+            },
+            1400: {
+                slidesPerView: 6,
+            },
+
+        }
+    });
+
+    $('.burger').click(function(){
+        $('.header__nav').addClass('show');
+    });
+
+    $('.close-menu').click(function(){
+        $('.header__nav').removeClass('show');
+    });
+    
+
+    if ($(window).width() < 992) {
+       $('.sublink > a').click(function(){
+        $(this).next('.submenu').slideToggle();
+       });
+    }
+
+
+
+
+
+
+
+});
